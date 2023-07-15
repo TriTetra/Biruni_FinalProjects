eeba06a3c7c48f2fb94aaf2940612cb3e0cc9356
Abdullokh Abdukodirov <55631365+TriTetra@users.noreply.github.com>
2023-07-15 22:18:12 +0300

    Delete ContactPage directory

--- ContactPage/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.contactpage {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.contactpage to javafx.fxml;
-    exports com.example.contactpage;
-}
